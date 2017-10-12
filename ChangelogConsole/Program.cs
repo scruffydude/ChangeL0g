7be7049e31578c96baba427e11667232b0653d36
Scruffy <Scruffy0616@gmail.com>
2017-10-12 16:37:39 -0400

    Cleaned up unnessesary usings.

--- ChangelogConsole/Program.cs
+++ ChangelogConsole/Program.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Engine;
+﻿using Engine;
+using System;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 
 namespace ChangelogConsole
 {
